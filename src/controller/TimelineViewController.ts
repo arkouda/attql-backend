6e4ba94b63aeeb98bf6551e184966ff4a2ef4b2e
Prabhtaj Singh Bhasin <burnwat3r@gmail.com>
2019-04-16 03:27:04 +0530

    Timeline Update

--- src/controller/TimelineViewController.ts
+++ src/controller/TimelineViewController.ts
@@ -1,6 +1,6 @@
-import {getRepository,SelectQueryBuilder} from "typeorm";
-import {NextFunction, Request, Response} from "express";
-import {Attendance} from "../entity/Attendance";
+import { getRepository, SelectQueryBuilder } from "typeorm";
+import { NextFunction, Request, Response } from "express";
+import { Attendance } from "../entity/Attendance";
 
 export class TimelineViewController {
 
@@ -9,22 +9,30 @@ export class TimelineViewController {
     async getTimeline(request: Request, response: Response) {
         var timelineViewData: Attendance[];
         var timelineViewDataQuery: SelectQueryBuilder<Attendance> = this.entityRepository
-                                .createQueryBuilder("a")
-                                .where("a.day >= :d", {d: 0});
-        if(request.query.dayLimit && request.query.page){
-            timelineViewDataQuery.andWhere('a.day >= :dayLimit_gte', {dayLimit_gte: (parseInt(request.query.page) - 1) * parseInt(request.query.dayLimit)});
-            timelineViewDataQuery.andWhere('a.day <= :dayLimit_lte', {dayLimit_lte: (parseInt(request.query.page)) * parseInt(request.query.dayLimit)});
+            .createQueryBuilder("a")
+            .where("a.day >= :d", { d: 0 });
+        if (request.query.dayLimit && request.query.page) {
+            timelineViewDataQuery.andWhere('a.day >= :dayLimit_gte', { dayLimit_gte: (parseInt(request.query.page) - 1) * parseInt(request.query.dayLimit) });
+            timelineViewDataQuery.andWhere('a.day <= :dayLimit_lte', { dayLimit_lte: (parseInt(request.query.page)) * parseInt(request.query.dayLimit) });
         }
         var group: object = {};
         var items: object = {};
         timelineViewData = await timelineViewDataQuery.getMany();
         timelineViewData.forEach((item: Attendance) => {
-            if(!group.hasOwnProperty(item.studid)) {
+            if (!group.hasOwnProperty(item.studid)) {
                 items[item.studid] = [];
                 group[item.studid] = 0;
             }
-            items[item.studid].push({day: item.day, arrivalTime: item.arrivalTime, departTime: item.departTime});
+            items[item.studid].push({ day: item.day, arrivalTime: item.arrivalTime, departTime: item.departTime });
         });
-        response.end(JSON.stringify({items: items, group: Object.keys(group)}));
+        var groupFinal: object[] = [];
+        Object.keys(group).forEach((item: string) => {
+            groupFinal.push({ id: item, content: "Student " + item });
+        });
+        var itemsFinal: object[] = [];
+        Object.keys(items).forEach((key) => {
+            itemsFinal.push({groupid: key, items: items[key]})
+        });
+        response.end(JSON.stringify({ items: itemsFinal, group: groupFinal }));
     }
 }
